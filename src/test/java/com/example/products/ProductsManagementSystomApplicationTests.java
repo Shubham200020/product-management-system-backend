ff172ab667c9166ac2db6665ebb10f13c0700100
Shubham Kumar <shubhammisra800@gmail.com>
2026-06-18 02:35:19 +0530

    Clean up and restore unit test class

--- src/test/java/com/example/products/ProductsManagementSystomApplicationTests.java
+++ src/test/java/com/example/products/ProductsManagementSystomApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ProductsManagementSystomApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
